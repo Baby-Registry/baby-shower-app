e689fee1edb85a058573c6ebedd9c570f3502677
Ryan Wong <ryan.wy.wong88@gmail.com>
2018-03-02 16:36:12 -0500

    built registry add/remove functionality

--- dev/scripts/productCard.js
+++ dev/scripts/productCard.js
@@ -25,9 +25,8 @@ class ProductCard extends React.Component {
             },
             xmlToJSON: false
             }}).then(({ data }) => {
-                // console.log(data.results[0]);
                 this.setState({
-                    imageURL:data.results[0].url_fullxfull
+                    imageURL:data.results[0].url_570xN
                 })
             })
         }
@@ -40,6 +39,7 @@ class ProductCard extends React.Component {
                 <h3>{`Price: ${this.props.data.currency_code} ${this.props.data.price}`}</h3>
                 <h3>{`Quantity: ${this.props.data.quantity} remaining`}</h3>
                 <a href={`${this.props.data.url}`}>Link to Item</a>
+                <button onClick={() => this.props.add(this.props.data)}>Add me!</button>
             </div>
         )
     }
---
+++ dev/scripts/registryList.js
@@ -0,0 +1,49 @@
+import React from "react";
+import variables from "./config.js";
+import axios from "axios";
+import Qs from 'qs';
+
+class RegistryList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            imgURL:""
+        }
+    }
+    componentDidMount () {
+        axios({
+            method: 'GET',
+            url: 'http://proxy.hackeryou.com',
+            dataResponse: 'json',
+            paramsSerializer: function (params) {
+                return Qs.stringify(params, { arrayFormat: 'brackets' })
+            },
+            params: {
+                reqUrl: `${variables.apiURL}/listings/${this.props.selection.listing_id}/images`,
+                params: {
+                    api_key: `${variables.apiKey}`
+                },
+                xmlToJSON: false
+            }
+        }).then(({ data }) => {
+            const results = data;
+            this.setState({
+                imageURL:data.results[0].url_570xN
+            })
+        })
+    }
+    render () {
+        return (
+            <div>
+                <img src={this.state.imageURL} alt="" />
+                <h2>{this.props.selection.title}</h2>
+                <h3>{`Price: ${this.props.selection.currency_code} ${this.props.selection.price}`}</h3>
+                <h3>{`Quantity: ${this.props.selection.quantity} remaining`}</h3>
+                <a href={`${this.props.selection.url}`}>Link to Item</a>
+                <button onClick={() => this.props.remove(this.props.selection.listing_id)}>Remove Item</button>
+            </div>
+        )
+    }
+}
+
+export default RegistryList;
