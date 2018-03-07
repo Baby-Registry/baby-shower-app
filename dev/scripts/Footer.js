87c60ab6efb5a5c39dd6a70a48cea1e161546dc7
Ryan Wong <ryan.wy.wong88@gmail.com>
2018-03-07 09:13:01 -0500

    change to footer and small change to button margin on registry list

--- dev/scripts/Footer.js
+++ dev/scripts/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer>
             <div className="wrapper">
                 <p>
-                    Credits: Created by <a href="https://twitter.com/ryanwong_code">Ryan Wang</a>, <a href="https://twitter.com/anastasiyacodes">Anastasiya Melentyeva</a> and <a href="https://twitter.com/Linda_OMG">Linda Zhao</a> 
+                    Credits: Created by <a href="https://twitter.com/ryanwong_code">Ryan Wong</a>, <a href="https://twitter.com/anastasiyacodes">Anastasiya Melentyeva</a> and <a href="https://twitter.com/Linda_OMG">Linda Zhao</a> 
                 </p>
             </div>
         </footer>
