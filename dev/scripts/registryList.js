d1f4796c7552fc110d7d2fa7c7053bff6b157be3
Ryan Wong <ryan.wy.wong88@gmail.com>
2018-03-09 16:49:07 -0500

    changes to registry list layout and responsiveness

--- dev/scripts/registryList.js
+++ dev/scripts/registryList.js
@@ -36,10 +36,10 @@ class RegistryList extends React.Component {
         return (
             <div className="savedRegistryList">
                 <img src={this.state.imageURL} alt="" />
-                <h3>{this.props.selection.title}</h3>
-                <h4>{`Price: ${this.props.selection.currency_code} ${this.props.selection.price}`}</h4>
-                <h4>{`Quantity: ${this.props.selection.quantity} remaining`}</h4>
-                <a href={`${this.props.selection.url}`}>More Details on Item</a>
+                {/* <h4>{this.props.selection.title}</h4> */}
+                {/* <h4>{`Price: ${this.props.selection.currency_code} ${this.props.selection.price}`}</h4> */}
+                {/* <h4>{`Quantity: ${this.props.selection.quantity} remaining`}</h4> */}
+                <a href={`${this.props.selection.url}`}>More Details</a>
                 <button onClick={() => this.props.remove(this.props.selection.listing_id)}>Remove Item</button>
                 {this.props.selection.purchase ? 
                     <h3>This has been purchased by a guest</h3>
